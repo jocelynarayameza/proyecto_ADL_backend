eb315b9d4bab6c27e2b3c02d358061364372799f
Millaray <millaray.painemil@outlook.cl>
2025-02-10 23:18:57 -0300

    Inicio backend

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+var cors = require('cors');
+
+const port = 3000;
+const routes = require('./routes/index');
+
+app.use(cors())
+app.use(express.json());
+app.use(express.urlencoded({ extendd: true }));
+app.use('/',routes());
+
+
+
+app.listen(port,() => console.log('Servidor encendido'));
