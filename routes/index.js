74e623ebcd02153498abedf45b89fcc5fd95df77
Millaray <millaray.painemil@outlook.cl>
2025-02-10 23:49:29 -0300

    Se agrego index, se elimino otras rutas

---
+++ routes/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const router = express.Router();
+const userController = require('../controllers/userController');
+const productController = require('../controllers/productController');
+const orderController = require('../controllers/orderController')
+const auth = require('../middlewares/auth')
+
+
+module.exports = () => {
+  router.get('/', (req,res) =>{
+    res.send('bienvenido')
+  })
+
+  // router.get('/usuarios',auth,usersController)
+  // router.post('/usuarios',usersController)
+  // router.post('/login',usuariosController)
+
+  router.get('*', (req,res) =>{
+    res.status(404).send({ msg: "La ruta que intenta consultar no existe" })
+  })
+
+  return router
+}
